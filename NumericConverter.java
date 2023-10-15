6703108a08d03d36b73471bedad9abee3d5f5fab
Cássio <cassiocr2012@hotmail.com>
2023-10-15 15:53:49 -0300

    Projeto de um conversor de número decimal em número binário e hexadecimal.

---
+++ NumericConverter.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class NumericConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número Decimal: ");
+        int numDecimal = scanner.nextInt();
+
+        System.out.println("Número decimal = " + numDecimal);
+        System.out.println("Número binário = " + decimalToBinary(numDecimal));
+        System.out.println("Número hexadecimal = " + decimalToHexadecimal(numDecimal));
+    }
+    private static String decimalToBinary(int numDecimal) {
+        if (numDecimal == 0) {
+            return "0";
+        } else if (numDecimal == 1) {
+            return "1";
+        } else {
+            int rest = numDecimal % 2;
+            int quotient = numDecimal / 2;
+            return decimalToBinary(quotient) + rest;
+        }
+    }
+    private static String decimalToHexadecimal(int numDecimal) {
+        if (numDecimal == 0) {
+            return "0";
+        } else {
+            int rest = numDecimal % 16;
+            int quotient = numDecimal / 16;
+            char digitHexadecimal = (rest < 10) ? (char) (rest + '0') : (char) (rest - 10 + 'A');
+            if (quotient == 0) {
+                return String.valueOf(digitHexadecimal);
+            } else {
+                return decimalToHexadecimal(quotient) + digitHexadecimal;
+            }
+        }
+    }
+}
